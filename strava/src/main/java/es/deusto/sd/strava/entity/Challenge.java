8930671206ba8ac3ba62f8f57f7def330e15a69d
SantiGaytan <s.gaytandeayala@opendeusto.es>
2025-04-09 16:13:07 +0200

    Challenge

--- strava/src/main/java/es/deusto/sd/strava/entity/Challenge.java
+++ strava/src/main/java/es/deusto/sd/strava/entity/Challenge.java
@@ -1,5 +1,11 @@
 package es.deusto.sd.strava.entity;
 
 public class Challenge {
+    private Long idChallenge;
+    private String name;
+    private Date startDate;
+    private Date endDate;
+    private Double objectiveValue;
+    private Sport sport;
     
 }

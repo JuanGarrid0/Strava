36e8de9352c62d8f39705b04204c6ebf888bd56c
JuanGarrid0 <114677191+JuanGarrid0@users.noreply.github.com>
2025-04-09 15:58:27 +0200

    endpoints

--- strava/src/main/java/es/deusto/sd/strava/facade/Controller.java
+++ strava/src/main/java/es/deusto/sd/strava/facade/Controller.java
@@ -7,9 +7,41 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class Controller {
 
+    @PostMapping("/login")      // login
+    public ResponseEntity<String> login(@RequestBody String name) {        
+        return new ResponseEntity<>("HelloWorld!!!", HttpStatus.OK);
+    }
+    @PostMapping("/logout")      // logout
+    public ResponseEntity<String> logout(@RequestBody String name) {        
+        return new ResponseEntity<>("HelloWorld!!!", HttpStatus.OK);
+    }
+    @PostMapping("/register")    // post registro user
+    public ResponseEntity<String> postRegister(@RequestBody String name) {
+        return new ResponseEntity<>("Hello, " + name + "!", HttpStatus.OK);
+    }
+    @PostMapping("/createSesion")    // post sesion
+    public ResponseEntity<String> postStravaSesion(@RequestBody String name) {
+        return new ResponseEntity<>("Hello, " + name + "!", HttpStatus.OK);
+    }
+    @PostMapping("/createChallenge")    // post reto
+    public ResponseEntity<String> postStravaChallenge(@RequestBody String name) {
+        return new ResponseEntity<>("Hello, " + name + "!", HttpStatus.OK);
+    }  
+    @PostMapping("/acceptChallenge")    // post challenge accepted
+    public ResponseEntity<String> postAcceptChallenge(@RequestBody String name) {
+        return new ResponseEntity<>("Hello, " + name + "!", HttpStatus.OK);
+    }
+    @GetMapping("/sesion")      // get sesion
+    public ResponseEntity<String> getSesions() {        
+        return new ResponseEntity<>("HelloWorld!!!", HttpStatus.OK);
+    }
+    @GetMapping("/challenges")      // get challenge
+    public ResponseEntity<String> getChallenges() {        
+        return new ResponseEntity<>("HelloWorld!!!", HttpStatus.OK);
+    }
+    @GetMapping("/acceptedChallenges")      // get challenges accepted
+    public ResponseEntity<String> getAcceptedChallenges() {        
+        return new ResponseEntity<>("HelloWorld!!!", HttpStatus.OK);
+    }
 
-@PostMapping("/strava")    // Server test
-public ResponseEntity<String> postStrava(@RequestBody String name) {
-    return new ResponseEntity<>("Hello, " + name + "!", HttpStatus.OK);
-}
 }

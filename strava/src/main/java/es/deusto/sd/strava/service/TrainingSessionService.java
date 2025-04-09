bddf028879b5b71f9cbf521ee775aaf0954ec73b
xabier.ceballos@opendeusto.es <xabier.ceballos@opendeusto.es>
2025-04-09 17:31:17 +0200

    Services

---
+++ strava/src/main/java/es/deusto/sd/strava/service/TrainingSessionService.java
@@ -0,0 +1,5 @@
+package es.deusto.sd.strava.service;
+
+public class TrainingSessionService{
+
+}
---
+++ strava/src/main/java/es/deusto/sd/strava/service/UserService.java
@@ -0,0 +1,7 @@
+package es.deusto.sd.strava.service;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    
+}
